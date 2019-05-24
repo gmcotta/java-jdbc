5a900f42d4e9a7eadbe29aa2d780bff50e66bd44
Gustavo Cotta <gmcotta34@gmail.com>
2019-05-23 22:24:53 -0300

    Demo: recuperar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,10 +11,27 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
 		
-		DB.closeConnection();
-
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT * FROM department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
 	}
 
 }
